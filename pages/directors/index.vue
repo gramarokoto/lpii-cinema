d6c2a34d0c0f9fe4d86cc7e50be7247ce58e3cde
gramarokoto <47677136+gramarokoto@users.noreply.github.com>
2022-05-06 19:59:53 +0200

    feat: add directors page

---
+++ pages/directors/index.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+  beforeCreate() {
+    // TODO : use middleware checking path == directors
+    this.$router.push({ name: 'directors-page-page', params: { page: 1 } })
+  },
+}
+</script>
